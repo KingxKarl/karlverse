66337bfd40342e83378b459edc58e56ea852981a
KingxKarl <150752668+KingxKarl@users.noreply.github.com>
2025-02-03 01:03:49 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 import jobRoutes from "./routes/jobRoutes.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Read environment variables from Azure
 
