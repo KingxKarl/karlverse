e178f8b66f1215e0cb0815412228577986e79742
KingxKarl <150752668+KingxKarl@users.noreply.github.com>
2025-02-09 21:50:20 -0500

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-const API_URL = import.meta.env.VITE_API_URL;
+// config.js
+const API_URL = import.meta.env.VITE_API_URL || "https://karlverse-bfdbcsbge7f5e6bh.eastus2-01.azurewebsites.net/api";
 export default API_URL;
